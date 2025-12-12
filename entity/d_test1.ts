547c1e0925f8399f9576e2b0449d118e9b0d6ab2
anirban444 <anirban@procifynow.com>
2025-12-12 19:04:07 +0530

    sdafasd

--- entity/d_test1.ts
+++ entity/d_test1.ts
@@ -1,6 +1,6 @@
 import { d_test1 as d_test1_gen } from "sample_app1530/entity_gen/d_test1";
 export class d_test1 extends d_test1_gen {
 	public customFunction(): void {
-		console.log("This is a custom function in d_test1");
+		console.log("anirban");
 	}
 }
